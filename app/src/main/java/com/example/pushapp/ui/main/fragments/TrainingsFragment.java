d4bb37e8dadf0f97d47141fe246bf977471b193d
EliaRaffaele <e.raffaeleaddamo@campus.unimib.it>
2025-12-14 18:09:17 +0100

    feat: added edit and delete buttons in trainings fragment.

--- app/src/main/java/com/example/pushapp/ui/main/fragments/TrainingsFragment.java
+++ app/src/main/java/com/example/pushapp/ui/main/fragments/TrainingsFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -15,9 +17,12 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.example.pushapp.R;
+import com.example.pushapp.models.Training;
 import com.example.pushapp.utils.TrainingListGenerator;
 import com.example.pushapp.utils.TrainingsAdapter;
 import com.example.pushapp.utils.TrainingsRecyclerViewAdapter;
+
+import java.util.ArrayList;
 
 public class TrainingsFragment extends Fragment {
 
@@ -29,17 +34,18 @@ public class TrainingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trainings, container, false);
+        View root = inflater.inflate(R.layout.fragment_trainings, container, false);
+        return root;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        TrainingsRecyclerViewAdapter adapter = new TrainingsRecyclerViewAdapter(
-                TrainingListGenerator.generateTrainingList());
 
+        ArrayList<Training> trainings = TrainingListGenerator.generateTrainingList();
+
+        TrainingsRecyclerViewAdapter adapter = new TrainingsRecyclerViewAdapter(trainings);
 
         RecyclerView recyclerView = view.findViewById(R.id.training_list);
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
--- app/src/main/java/com/example/pushapp/utils/TrainingsRecyclerViewAdapter.java
+++ app/src/main/java/com/example/pushapp/utils/TrainingsRecyclerViewAdapter.java
@@ -1,42 +1,74 @@
 package com.example.pushapp.utils;
 
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pushapp.R;
 import com.example.pushapp.models.Training;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
 
 public class TrainingsRecyclerViewAdapter
         extends RecyclerView.Adapter<TrainingsRecyclerViewAdapter.ViewHolder> {
-
     private final ArrayList<Training> trainings;
 
     public TrainingsRecyclerViewAdapter(ArrayList<Training> trainings) {
         this.trainings = trainings;
     }
+
+
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private final TextView textViewName;
-        private final TextView textViewDescription;
+        private final TextInputEditText textInputName;
+        private final TextInputEditText textInputDescription;
+        private final ImageButton editButton;
+        private final ImageButton deleteButton;
+
+        // tells if the user is editing the text fields in the card
+        private boolean isEditing = false;
 
 
         public ViewHolder(View view) {
             super(view);
-            textViewName= (TextView) view.findViewById(R.id.text_view_name);
-            textViewDescription= (TextView) view.findViewById(R.id.text_view_description);
+            textInputName = view.findViewById(R.id.text_view_name);
+            textInputDescription = view.findViewById(R.id.text_view_description);
+            editButton = view.findViewById(R.id.edit_image_button);
+            deleteButton = view.findViewById(R.id.delete_image_button);
         }
 
-        public TextView getTextViewName() {
-            return textViewName;
+        public TextInputEditText getTextViewName() {
+            return textInputName;
         }
 
-        public TextView getTextViewDescription() {
-            return textViewDescription;
+        public TextInputEditText getTextViewDescription() {
+            return textInputDescription;
+        }
+
+        public ImageButton getEditButton() {
+            return editButton;
+        }
+
+        public ImageButton getDeleteButton() {
+            return deleteButton;
+        }
+
+        public boolean isEditing() {
+            return isEditing;
+        }
+
+        public void setEditing(boolean editing) {
+            isEditing = editing;
         }
     }
 
@@ -53,15 +85,58 @@ public class TrainingsRecyclerViewAdapter
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, final int position) {
 
-        // Get element from your dataset at this position and replace the
-        // contents of the view with that element
         viewHolder.getTextViewName().setText(trainings.get(position).getName());
         viewHolder.getTextViewDescription().setText(trainings.get(position).getDescription());
+
+        viewHolder.getEditButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                handleEditButtonClick(viewHolder);
+            }});
+
+        viewHolder.getDeleteButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                handleDeleteButtonClick(viewHolder);
+            }});
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
         return trainings.size();
+    }
+
+    private void handleEditButtonClick(ViewHolder viewHolder){
+        if(viewHolder.isEditing()) {
+            viewHolder.getTextViewName().setFocusable(false);
+            viewHolder.getTextViewDescription().setFocusable(false);
+            viewHolder.getTextViewName().setFocusableInTouchMode(false);
+            viewHolder.getTextViewDescription().setFocusableInTouchMode(false);
+            viewHolder.setEditing(false);
+            viewHolder.getEditButton().setImageResource(R.drawable.edit);
+
+        } else {
+            viewHolder.getTextViewName().setFocusable(true);
+            viewHolder.getTextViewDescription().setFocusable(true);
+            viewHolder.getTextViewName().setFocusableInTouchMode(true);
+            viewHolder.getTextViewDescription().setFocusableInTouchMode(true);
+            viewHolder.setEditing(true);
+            viewHolder.getEditButton().setImageResource(R.drawable.check);
+        }
+    }
+
+    private void handleDeleteButtonClick(ViewHolder viewHolder) {
+        new MaterialAlertDialogBuilder(viewHolder.itemView.getContext())
+                .setTitle(R.string.confirm_operation)
+                .setMessage(R.string.are_you_sure_delete)
+                .setPositiveButton(R.string.confirm, (dialog, which) -> {
+                    dialog.dismiss();
+                })
+                .setNegativeButton(R.string.cancel, (dialog, which) -> {
+                    dialog.dismiss();
+                })
+                .show();
+
     }
 }
